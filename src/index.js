cf86efcc5d209c874e8a1c30b4601c7a5164da82
Garrett Halstein <garrett.halstein@emory.edu>
2019-07-17 17:14:02 -0400

    finished the filter

--- src/index.js
+++ src/index.js
@@ -1,6 +1,34 @@
 console.log('%c HI', 'color: firebrick')
-
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+function fetchBreeds() {
+	fetch(breedUrl)
+	.then(resp => resp.json())
+	.then(json => postBreed(json));
+}
+
+function postBreed(json) {
+	let breedList = document.querySelector("#dog-breeds");
+	for (let key in json.message) {
+		let breed = document.createElement('li');
+		if (json.message[key].length > 0) {
+			json.message[key].forEach(e => {
+				breed.innerHTML = `${e} ${key}`;
+				breed.className = "breed";
+				breedList.append(breed);
+			})
+		}
+		else {
+			breed.innerHTML = `${key}`;
+			breed.className = "breed";
+			breedList.append(breed);
+		}
+	}
+	breedList.addEventListener('click', e => {
+		e.target.style.color = 'blue';
+	});
+}
 
 function fetchImages() {
 	fetch(imgUrl)
@@ -20,6 +48,26 @@ function postImage(json) {
 	});
 }
 
+function filter() {
+	let breedDropDown = document.querySelector("#breed-dropdown");
+	breedDropDown.addEventListener('change', e => {
+		document.querySelectorAll('.breed').forEach(element => {
+			// debugger
+			if (element.innerText.toLowerCase().charAt(0) == e.target.value) {
+				element.style.display = "block";
+			}
+			else {
+				element.style.display = "none";
+			}
+		})
+	})
+}
+
 document.addEventListener('DOMContentLoaded', e => {
 	fetchImages();
-}); 
+	fetchBreeds();
+	filter();
+});
+
+
+

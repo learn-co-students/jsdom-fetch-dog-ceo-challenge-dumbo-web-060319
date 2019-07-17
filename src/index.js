9d911bc90a58187a17575009d5acfc553d1f8e62
Alexander Schelchere <ascellie@gmail.com>
2019-07-17 12:11:51 -0400

    second challenge

--- src/index.js
+++ src/index.js
@@ -1,27 +1,66 @@
 console.log('%c HI', 'color: firebrick');
 // URL
-const imgUrl = 'https://dog.ceo/api/breeds/image/random/4';
+const dogImgUrl = 'https://dog.ceo/api/breeds/image/random/4';
+const dogBreedUrl = 'https://dog.ceo/api/breeds/list/all';
 
 // Function to fetch the data
-function getDoggieData() {
-  fetch(imgUrl)
+function getDoggieData(url, dogMethod) {
+  fetch(url)
     .then(resp => resp.json())
-    .then(data => getDogsOnPage(data));
+    .then(data => dogMethod(data));
 }
 
 // Function to load the data onto the page
-function getDogsOnPage(json) {
+function getDogPicturesOnPage(json) {
   // Grab DOM elements
-  let dogContainer = document.querySelector('#dog-breeds');
+  let dogImageContainer = document.querySelector('#dog-image-container');
 
   let doggies = json['message'];
   for (const dogLink of doggies) {
-    let dog = document.createElement('li');
-    dog.innerHTML = `<img src=${dogLink} alt="dog image">`;
-    dogContainer.append(dog);
+    let dog = document.createElement('img');
+    dog.src = dogLink;
+    dog.alt = 'random dog image';
+    dog.class = 'random-dog-image';
+    dog.style.height = '360px';
+    dogImageContainer.append(dog);
+  }
+}
+
+function getDogBreedsOnPage(json) {
+  // grab UL
+  let dogBreedContainer = document.querySelector('#dog-breeds');
+  let doggieBreeds = json['message'];
+  for (const dogBreedKey in doggieBreeds) {
+    if (doggieBreeds.hasOwnProperty(dogBreedKey)) {
+      // debugger;
+
+      // Get the value from the key
+      const breedList = doggieBreeds[dogBreedKey];
+
+      // if the value is an empty array
+      if (breedList.length == 0) {
+        // Create element
+        let dogBreed = document.createElement('li');
+        // make the list item's text the name of the key
+        dogBreed.innerText = dogBreedKey;
+        // append breed to dogBreed container
+        dogBreedContainer.append(dogBreed);
+      } else {
+        // loop through array
+        for (const subBreed of breedList) {
+          // Create element
+          let dogBreed = document.createElement('li');
+          // make the list item's text the name of the key And value
+          dogBreed.innerText = `${subBreed} ${dogBreedKey}`;
+          // append breed to dogBreed container
+          dogBreedContainer.append(dogBreed);
+        }
+      }
+    }
   }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
-  getDoggieData();
+  getDoggieData(dogImgUrl, getDogPicturesOnPage);
+  getDoggieData(dogBreedUrl, getDogBreedsOnPage);
 });

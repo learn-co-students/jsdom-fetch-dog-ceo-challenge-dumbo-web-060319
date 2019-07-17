f268c16d66c74a32d6998ab6d4924f0d29ae9253
Garrett Halstein <garrett.halstein@emory.edu>
2019-07-17 14:54:58 -0400

    got the images to show up

--- src/index.js
+++ src/index.js
@@ -1,1 +1,25 @@
 console.log('%c HI', 'color: firebrick')
+
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
+
+function fetchImages() {
+	fetch(imgUrl)
+	.then(resp => resp.json())
+	.then(json => postImage(json));
+}
+
+
+function postImage(json) {
+	let dogImgContainer = document.querySelector("#dog-image-container");
+	let listOfImages = document.createElement('ul');
+	dogImgContainer.append(listOfImages);
+	json.message.forEach(e => {
+		let item = document.createElement('li');
+		item.innerHTML = `<img src= ${e}>`;
+		listOfImages.append(item);
+	});
+}
+
+document.addEventListener('DOMContentLoaded', e => {
+	fetchImages();
+}); 
